08183e8b796fb141b3d6ce10102f8de081c905f7
Sebi <sebialfonso08@outlook.com>
2026-06-15 20:45:18 -0400

    Task 2: Implemented FizzBuzz from 1 to 50 using conditional statements and loops

---
+++ task2.lua
@@ -0,0 +1,11 @@
+for i = 1, 50 do
+    if i % 3 == 0 and i % 5 == 0 then
+        print("FizzBuzz")
+    elseif i % 3 == 0 then
+        print("Fizz")
+    elseif i % 5 == 0 then
+        print("Buzz")
+    else
+        print(i)
+    end
+end
